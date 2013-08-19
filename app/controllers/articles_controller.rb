13311c3dc32ca150edee500732bed8145c01f9ab
tetesting <tetestingtesting@gmail.com>
2013-08-18 20:27:17 -0400

    add "new article" view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,10 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find params[:id]
   end
+  def new
+    @article = Article.new
+  end
+  def create
+    
+  end
 end
