6e60257116f1b94ab18209fff5f283086ab3566f
tetesting <tetestingtesting@gmail.com>
2013-08-18 20:40:32 -0400

    add article redirection in create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,8 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new
     @article.title = params[:article][:title]
+    @article.body = params[:article][:body]
     @article.save
+    redirect_to article_path @article
   end
 end
