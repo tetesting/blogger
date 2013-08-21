1078d2bb3acde7a7058c709387b5788fcb4ff4c3
tetesting <tetestingtesting@gmail.com>
2013-08-21 11:49:56 -0400

    add delete & destroy functions to article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,12 +8,20 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+
   def create
     @article = Article.new(article_params)
     @article.save
     redirect_to article_path @article
   end
-  def article_params
-    params.require(:article).permit(:title, :body)
+  def destroy
+      @article = Article.find params[:id]
+      @article.destroy
+      redirect_to articles_path
   end
 end
