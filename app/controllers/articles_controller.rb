fdd0385b752ef3b37e666ffe9b202579fc70a67d
tetesting <tetestingtesting@gmail.com>
2013-08-21 12:49:06 -0400

    articles: create form partial ; edit function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,4 +24,13 @@ class ArticlesController < ApplicationController
       @article.destroy
       redirect_to articles_path
   end
+  def edit
+      @article = Article.find(params[:id])
+  end
+  def update
+      @article = Article.find(params[:id])
+      @article.update article_params 
+      
+      redirect_to article_path(@article)
+  end
 end
